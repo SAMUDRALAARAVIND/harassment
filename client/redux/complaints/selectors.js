3be8875b8d1c4075500f8ce4fa8fa1f9e13ea092
SAMUDRALAARAVIND <aravindsamudrala1708@gmail.com>
2025-10-09 22:44:12 +0530

    cleanup

--- client/redux/complaints/selectors.js
+++
@@ -1,4 +0,0 @@
-export const getComplaintsForActivePage = (state) => {
-  const pageNumber = state.complaints.pagination.activePage ?? 1;
-  return state.complaints.data[pageNumber.toString()] ?? [];
-};
